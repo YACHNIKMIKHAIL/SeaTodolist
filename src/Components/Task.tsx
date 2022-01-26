77764139a8841a26afea38db55e08d418d41e493
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-01-26 17:36:27 +0300

    bla bla commit

--- src/Components/Task.tsx
+++ src/Components/Task.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import EditSpan from "./EditSpan";
 import {IconButton} from "@material-ui/core";
 import {Delete} from "@material-ui/icons";
@@ -11,31 +11,31 @@ type TaskPropsType = {
     id: string
     todolistID: string
 }
-const Task = ({todolistID, id}: TaskPropsType) => {
-    const actualTask = useSelector<reducerType, TaskType>(state => state.tasks[todolistID].filter(f => f.id === id)[0])
-    const dispatch = useDispatch()
+const Task = React.memo(({todolistID, id}: TaskPropsType) => {
+        const actualTask = useSelector<reducerType, TaskType>(state => state.tasks[todolistID].filter(f => f.id === id)[0])
+        const dispatch = useDispatch()
 
-    const changeTaskTitle = (title: string) => {
-        dispatch(changeTaskTitleAC(todolistID, id, title))
-    }
-    const removeTask = () => {
-        dispatch(removeTaskAC(todolistID, id))
-    }
-    const changeTaskStatus = (isDone: boolean) => {
-        dispatch(changeTaskStatusAC(todolistID, id, isDone))
-    }
+        const changeTaskTitle = useCallback((title: string) => {
+            dispatch(changeTaskTitleAC(todolistID, id, title))
+        }, [dispatch,todolistID, id])
+        const removeTask = useCallback(() => {
+            dispatch(removeTaskAC(todolistID, id))
+        }, [dispatch,todolistID, id])
+        const changeTaskStatus = useCallback((isDone: boolean) => {
+            dispatch(changeTaskStatusAC(todolistID, id, isDone))
+        }, [dispatch,todolistID, id])
 
-    return (
-        <div>
-            <input type="checkbox" checked={actualTask.isDone}
-                   onChange={(e) => changeTaskStatus(e.currentTarget.checked)}/>
-            <EditSpan title={actualTask.title} callback={changeTaskTitle} id={actualTask.id}
-                      isDone={actualTask.isDone}/>
-            <IconButton aria-label="delete" onClick={removeTask}>
-                <Delete/>
-            </IconButton>
-        </div>
-    );
-};
-
+        return (
+            <div>
+                <input type="checkbox" checked={actualTask.isDone}
+                       onChange={(e) => changeTaskStatus(e.currentTarget.checked)}/>
+                <EditSpan title={actualTask.title} callback={changeTaskTitle} id={actualTask.id}
+                          isDone={actualTask.isDone}/>
+                <IconButton aria-label="delete" onClick={removeTask}>
+                    <Delete/>
+                </IconButton>
+            </div>
+        );
+    }
+)
 export default Task;
